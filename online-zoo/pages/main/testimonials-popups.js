08f29fccb54ac36da4c1e19fcd598a1daa3a69d1
Olga Dementey <dementeyolya@gmail.com>
2022-10-06 08:08:18 +0300

    feat: finish popup

--- online-zoo/pages/main/testimonials-popups.js
+++ online-zoo/pages/main/testimonials-popups.js
@@ -1,3 +1,18 @@
 const reviews = document.querySelectorAll(
-  ".testimonials__cards .testimonials__card"
-);
+		".testimonials__cards .testimonials__card"
+	),
+	popups = document.querySelectorAll(".testimonials__popup"),
+	popupReviews = document.querySelectorAll(
+		".testimonials__popup .testimonials__card"
+	),
+	popupCloseIcons = document.querySelectorAll(".testimonials__popup-cross");
+
+for (let i = 0; i < popups.length; i++) {
+	reviews[i].onclick = () => popups[i].classList.toggle("active");
+	popupCloseIcons[i].onclick = () => popups[i].classList.toggle("active");
+	popups[i].onclick = (event) => {
+		if (event.target === popups[i]) {
+			popups[i].classList.toggle("active");
+		}
+	};
+}
