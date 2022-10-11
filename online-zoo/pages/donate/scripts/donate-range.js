af8e9b04dee72dd02099396f45720ff211100ade
Olga Dementey <dementeyolya@gmail.com>
2022-10-11 08:09:24 +0300

    feat: finish donate range and amount input

--- online-zoo/pages/donate/scripts/donate-range.js
+++ online-zoo/pages/donate/scripts/donate-range.js
@@ -1,8 +1,29 @@
 let donateRange = document.querySelector('#range__input-desktop'),
-	donateNumImput = document.querySelector('.help__amount-input');
+	donateNumInput = document.querySelector('.help__amount-input'),
+	donateSums = document.querySelectorAll('.help__range-number');
 
 if (document.body.offsetWidth <= 840) {
 	donateRange.max = '5';
 } else if (document.body.offsetWidth <= 1000) {
 	donateRange.max = '7';
 }
+
+let arrayOfSums = [];
+
+for (let i = 0; i < donateSums.length; i++) {
+	let inputContent = donateSums[i].textContent;
+	arrayOfSums.push(inputContent.substring(1));
+}
+
+donateRange.addEventListener('input', function () {
+	donateNumInput.value = arrayOfSums[donateRange.value];
+});
+
+donateNumInput.addEventListener('input', function () {
+	if (arrayOfSums.includes(donateNumInput.value)) {
+		donateRange.value = arrayOfSums.indexOf(donateNumInput.value);
+	}
+});
+
+donateRange.value = donateRange.max - 2;
+donateNumInput.value = arrayOfSums[donateRange.max - 2];
