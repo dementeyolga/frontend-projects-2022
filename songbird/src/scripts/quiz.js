12ac096843cd4bd9e4d175e52ac7c4057197e7fd
Olga Dementey <dementeyolya@gmail.com>
2022-11-16 17:31:45 +0300

    feat: add question rendering

--- songbird/src/scripts/quiz.js
+++ songbird/src/scripts/quiz.js
@@ -1,5 +1,11 @@
 import './../styles/pages/quiz.scss';
+import defaultBirdPicture from './../assets/images/default-bird-img.jpg';
 import birdsData from './birds/birds-data-ru';
+const questionBody = document.querySelector('.question__body');
+const questionAnswers = document.querySelector('.question__answers');
+const questionBirdDescription = document.querySelector(
+  '.question__bird-description'
+);
 
 console.log(birdsData);
 
@@ -9,6 +15,65 @@ function chooseRandomIndex(i) {
   return randInd;
 }
 
+function createAnswers(arrOfBirdsInfo) {
+  const rightAnswer =
+    arrOfBirdsInfo[chooseRandomIndex(arrOfBirdsInfo.length)].name;
+
+  const arrOfAnswers = [];
+  const birdsNames = arrOfBirdsInfo.map((item) => item.name);
+
+  while (birdsNames.length) {
+    let randInd = chooseRandomIndex(birdsNames.length);
+    let randBird = birdsNames.splice(randInd, 1)[0];
+    let answer = {
+      name: randBird,
+      right: randBird === rightAnswer ? true : false,
+    };
+
+    arrOfAnswers.push(answer);
+  }
+
+  return arrOfAnswers;
+}
+
+function renderQuestion(arrOfBirdsInfo) {
+  let arrOfAnswers = createAnswers(arrOfBirdsInfo);
+  let rightAnswer = arrOfAnswers.find((item) => item.right).name;
+  let rightBirdInfo = arrOfBirdsInfo.find((item) => item.name === rightAnswer);
+
+  console.log(rightAnswer, rightBirdInfo);
+
+  let questionBodyHTML = `
+        <div class="question__body-picture">
+          <img src="${defaultBirdPicture}">
+        </div> 
+
+        <div class="question__body-content">
+          <div class="question__body-name">****</div>
+          <div class="question__body-audio">
+            <audio controls src="${rightBirdInfo.audio}">
+            </audio>
+          </div>
+        </div>
+  `;
+
+  questionBody.innerHTML = questionBodyHTML;
+
+  let questionAnswersHTML = '';
+
+  for (let i = 0; i < arrOfAnswers.length; i++) {
+    console.log(arrOfAnswers[i]);
+    questionAnswersHTML += `
+    <input class="question__answers-input" type="radio" name="bird-answer" id="bird-answer-${i}"/> 
+    <label for="bird-answer-${i}" class="question__answers-label">
+      ${arrOfAnswers[i].name}
+    </label> 
+    `;
+  }
+
+  questionAnswers.innerHTML = questionAnswersHTML;
+}
+
 const firstGroup = birdsData[0];
 
-const questionHTML = firstGroup.reduce((html, item) => {});
+renderQuestion(firstGroup);
