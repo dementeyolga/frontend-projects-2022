6f59be1518e651af3e040ad1878276e3719c220e
Olga Dementey <dementeyolya@gmail.com>
2022-12-12 15:37:56 +0300

    feat: improve environment configuration (eslint, webpack configs)

--- migration-to-typescript/webpack.config.js
+++ migration-to-typescript/webpack.config.js
@@ -2,9 +2,10 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index'),
     mode: 'development',
     module: {
         rules: [
@@ -17,23 +18,10 @@ const baseConfig = {
                 use: 'ts-loader',
                 include: [path.resolve(__dirname, 'src')],
             },
-            {
-                test: /\.ts$/,
-                enforce: 'pre',
-                use: [
-                    {
-                        options: {
-                            eslintPath: require.resolve('eslint'),
-                        },
-                        loader: require.resolve('eslint-loader'),
-                    },
-                ],
-                exclude: /node_modules/,
-            },
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.ts', '.js'],
     },
     output: {
         filename: 'index.js',
@@ -45,6 +33,7 @@ const baseConfig = {
             filename: 'index.html',
         }),
         new CleanWebpackPlugin(),
+        new EslingPlugin({ extensions: 'ts' }),
     ],
 };
 
