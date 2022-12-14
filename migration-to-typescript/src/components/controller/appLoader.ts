f88e001dbbbab466678cd1fa4fda07c9a1acb46a
Olga Dementey <dementeyolya@gmail.com>
2022-12-14 21:38:08 +0300

    feat: change API link

--- migration-to-typescript/src/components/controller/appLoader.ts
+++ migration-to-typescript/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://nodenews.up.railway.app/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: 'a198a99f52de4edf8084d422ea4af96a', // News API key
         });
     }
